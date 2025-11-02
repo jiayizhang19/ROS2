b8ea978889df87912d4c9183beed0fefbb6eb25a
Jiayi Zhang <j.zhang19@outlook.com>
2025-11-02 16:49:52 +0000

    1.update the straight line movement strategy in a new way. 2. add the basic and safest freestyle movement for any reachable points. 3. enable movement strategy selection by the user.

--- Assignment_1/src/rx200_moveit_control/rx200_moveit_control/rx200_moveit_action_client.py
+++ Assignment_1/src/rx200_moveit_control/rx200_moveit_control/rx200_moveit_action_client.py
@@ -25,7 +25,6 @@ class MoveItEEClient(Node):
         self.ee_link = 'rx200/ee_gripper_link'
         self.base_link = 'rx200/base_link'
         self.gripper_joint = 'left_finger'
-        self.safe_pose = (0.15, 0.0, 0.25) # define a safe pose
 
         self.yaml_dir = os.path.join(
             get_package_share_directory('rx200_moveit_control'),
@@ -33,7 +32,13 @@ class MoveItEEClient(Node):
         )
 
         self.poses = self.load_from_yaml(self.yaml_dir, 'poses.yaml')
-
+        self.linear_move_enabled = self.poses['linear_move']['enabled']
+        self.linear_move_steps = self.poses['linear_move']['steps']
+        self.safe_pose = (
+            self.poses['safe_pose']['x'],
+            self.poses['safe_pose']['y'],
+            self.poses['safe_pose']['z']
+        )
         # Another way to read parameters from config file
         # self.declare_parameters(
         #     namespace='',
@@ -55,8 +60,15 @@ class MoveItEEClient(Node):
 
         self.get_logger().info('Node initialized successfully!')
         self.get_logger().info(f'start_pose: {start_pose}, target_pose: {target_pose}')
-        self.move(start_pose, target_pose)
+        self.movement_strategy()
     
+
+    def load_from_yaml(self, dir, file):
+        path = os.path.join(dir, file)
+        print(f'Loading poses from: {path}')
+        with open(path, 'r') as file:
+            return yaml.safe_load(file)
+
 
     def get_start_target_poses(self):
         x_start = self.poses['start_point']['x']
@@ -72,6 +84,75 @@ class MoveItEEClient(Node):
     
     def get_gripper_state(self):
         return self.poses['gripper']['start_gripper_state']
+
+
+    def movement_strategy(self):
+        if self.linear_move_enabled:
+            self.linear_move(self.linear_move_steps)
+        else:
+            self.free_move()
+
+
+    def free_move(self):
+        start, target = self.get_start_target_poses()
+        self.send_ee_pose(*self.safe_pose)  # move to a safe place first
+        self.send_ee_pose(*start)  # move to start pose
+        self.send_gr_pose(open=True)  # open gripper
+        self.send_gr_pose(open=False)  # close gripper
+        self.send_ee_pose(*self.safe_pose)  # move to a safe place
+        self.send_ee_pose(*target)  # move to target pose
+        self.send_gr_pose(open=True)  # open gripper
+        self.send_ee_pose(*self.safe_pose)  # move to a safe place
+
+
+    def linear_move(self, steps):
+        (x_start, y_start, z_start), (x_target, y_target, z_target) = self.get_start_target_poses()
+        start_gripper_state = self.get_gripper_state()
+        z_inc = 0.05
+        z_start_up = z_start + z_inc
+        z_target_up = z_target + z_inc
+        waypoints = self.linear_waypoints(
+            (x_start, y_start, z_start_up),
+            (x_target, y_target, z_target_up),
+            steps
+        )
+        self.send_gr_pose(start_gripper_state)
+        self.send_ee_pose(*self.safe_pose)  # Start from a safe pose
+        self.send_ee_pose(x_start, y_start, z_start_up) # Move above the start pose
+        self.send_ee_pose(x_start, y_start, z_start)  # Move to the start pose
+        self.send_gr_pose(False)  # Close gripper
+        self.send_ee_pose(x_start, y_start, z_start_up) # Move back up
+        for waypoint in waypoints:
+            x, y, z = waypoint
+            self.get_logger().info(f'Moving to: ({x:.3f} {y:.3f} {z_start_up:.3f})') 
+            self.send_ee_pose(x, y, z)
+        self.send_ee_pose(x_target, y_target, z_target)  # Lower to the goal pose
+        self.send_gr_pose(True)  # Open gripper
+
+
+    def linear_waypoints(self, start, goal, steps):
+        waypoints = []
+        for step in range(steps + 1):
+            increment = step / steps
+            x = start[0] + increment * (goal[0] - start[0])
+            y = start[1] + increment * (goal[1] - start[1])
+            z = start[2] + increment * (goal[2] - start[2])
+            waypoints.append((x, y, z))
+        return waypoints 
+
+
+    def compute_quaternion(self, target, start=(0.0, 0.0, 0.0)):
+        '''
+        Compute quaternion from start and target positions in the X-Y plane
+        > Outputs: Quaternion (x, y, z, w) in radians
+        '''
+        x = target[0] - start[0]
+        y = target[1] - start[1]
+        # compute the angle between two points in the X-Y plane, which is the yaw angle
+        yaw = math.atan2(y, x) 
+        # convert euler angles to quaternion
+        return quaternion_from_euler(0.0, 0.0, yaw)
+    
 
     def send_ee_pose(self, x, y, z, task=None):
         pose = PoseStamped()
@@ -124,7 +205,8 @@ class MoveItEEClient(Node):
             goal, 
             feedback_callback=self._feedback_cb
             )
-        send_future.add_done_callback(self._goal_response_cb)
+        send_future.add_done_callback(self._target_response_cb)
+
 
     def send_gr_pose(self, open=True):
         req = MotionPlanRequest()
@@ -148,9 +230,10 @@ class MoveItEEClient(Node):
         goal.planning_options.plan_only = False 
 
         send_future = self._client.send_goal_async(goal)
-        send_future.add_done_callback(self._goal_response_cb)
+        send_future.add_done_callback(self._target_response_cb)
 
-    def _goal_response_cb(self, future):
+
+    def _target_response_cb(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().error('MoveIt goal rejected')
@@ -158,43 +241,18 @@ class MoveItEEClient(Node):
         self.get_logger().info('MoveIt goal accepted')
         goal_handle.get_result_async().add_done_callback(self._result_cb)
 
+
     def _feedback_cb(self,feedback_msg):
         state = getattr(feedback_msg.feedback, 'state', '<unknown>')
         self.get_logger().info(f'[Feedback] {state}')
+
 
     def _result_cb(self, future, task=None):
         result = future.result().result
         code = getattr(result.error_code, 'val', 'unknown')
         self.get_logger().info(f'[{task} - Result] error_code {code}')
 
-    def move(self, start, target):
-        self.send_ee_pose(*self.safe_pose)  # move to a safe place first
-        self.send_ee_pose(*start)  # move to start pose
-        self.send_gr_pose(open=True)  # open gripper
-        self.send_gr_pose(open=False)  # close gripper
-        self.send_ee_pose(*self.safe_pose)  # move to a safe place
-        self.send_ee_pose(*target)  # move to target pose
-        self.send_gr_pose(open=True)  # open gripper
-        self.send_ee_pose(*self.safe_pose)  # move to a safe place
 
-    def compute_quaternion(self, target, start=(0.0, 0.0, 0.0)):
-        '''
-        Compute quaternion from start and target positions in the X-Y plane
-        > Outputs: Quaternion (x, y, z, w) in radians
-        '''
-        x = target[0] - start[0]
-        y = target[1] - start[1]
-        # compute the angle between two points in the X-Y plane, which is the yaw angle
-        yaw = math.atan2(y, x) 
-        # convert euler angles to quaternion
-        return quaternion_from_euler(0.0, 0.0, yaw)
-    
-
-    def load_from_yaml(self, dir, file):
-        path = os.path.join(dir, file)
-        print(f'Loading poses from: {path}')
-        with open(path, 'r') as file:
-            return yaml.safe_load(file)
     
 
 
